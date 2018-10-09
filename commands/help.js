f7c9c6bbd91975c80ea41c35312e860133c0a784
kbalazschallenge <43724499+kbalazschallenge@users.noreply.github.com>
2018-10-09 18:16:25 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,11 +5,7 @@ module.exports.run = async (bot, message, args) => {
     let info = new Discord.RichEmbed()
     .setTitle("Segítség")
     .setColor("#80f442")
-    .addField("Parancsok:")
-    .addField("!kick")
-    .addField("!ban")
-    .addField("!tempmute")
-    .addField("!addrole <@felhasználó> <role>");
+    .addField("Parancsok:", `!kick - Megjelölt felhasználó kirúgása.\n!ban - Megjelölt fehasználó kitiltása.\n!tempmute - Megjelölt felhasználó némítása.\n!addrole - Megjelölt felhasználónak egy rang adása.`)
 
     return message.channel.send(info);
 }
