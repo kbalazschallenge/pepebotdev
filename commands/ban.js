aec5d18597a1842e6248f9708dc56acbc14d3248
kbalazschallenge <43724499+kbalazschallenge@users.noreply.github.com>
2018-10-09 18:28:29 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return message.channel.send("Nem találom a felhasználót...");
+   // if(!bUser) return message.channel.send("Nem találom a felhasználót...");
     let bReason = args.join(" ").slice(22);
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Ehhez nincs jogod!");
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Őt nem rúghatod ki!");
